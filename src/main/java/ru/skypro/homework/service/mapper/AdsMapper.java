f960bbf88a182e9538427ab984e805e8cf443f0e
Денис <den_2005@list.ru>
2023-02-12 22:50:41 +0300

    AdsCreateDto

--- src/main/java/ru/skypro/homework/service/mapper/AdsMapper.java
+++ src/main/java/ru/skypro/homework/service/mapper/AdsMapper.java
@@ -2,14 +2,14 @@ package ru.skypro.homework.service.mapper;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapping;;
+import ru.skypro.homework.dto.adsDto.AdsCreateDto;
 import ru.skypro.homework.dto.adsDto.AdsDto;
 import ru.skypro.homework.model.Ads;
 import ru.skypro.homework.model.Images;
 import ru.skypro.homework.model.Users;
+
+import java.time.Instant;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface AdsMapper {
@@ -17,4 +17,6 @@ public interface AdsMapper {
     @Mapping(source = "images.image", target = "image")
     @Mapping(source = "users.id", target = "author")
     AdsDto toDto(Ads ads, Images images, Users users);
+
+    Ads toModel(AdsCreateDto dto);
 }
